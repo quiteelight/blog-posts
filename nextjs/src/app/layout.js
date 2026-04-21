23aa942d7ccc69547f764de3cdb5ed0a7a6d487d
quiteelight <quiteelighti@gmail.com>
2026-04-21 16:26:17 +0300

    Nextjs basic setup

---
+++ nextjs/src/app/layout.js
@@ -0,0 +1,11 @@
+import "./globals.css"
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ nextjs/src/app/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-zinc-900 font-sans p-8 text-center">
+      <h1 className="text-3xl font-bold mb-6 text-white">
+        INFOSIVU
+      </h1>
+
+      <p className="text-zinc-300 mb-4">
+        Avaa selaimessa jokin seuraavista osoitteista:
+      </p>
+
+      <div className="space-y-2 text-lg font-mono text-zinc-200">
+        <p>/csr</p>
+        <p>/ssr</p>
+        <p>/ssg</p>
+        <p>/isr</p>
+      </div>
+    </div>
+  );
+}
